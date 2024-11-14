cffebbe48453c0d6f40237f575215ee904ab9362
Jbub5 <133902109+Jbub5@users.noreply.github.com>
2024-11-14 03:27:52 +0300

    voltageos

--- voltage_lancelot.mk
+++ voltage_lancelot.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/lancelot/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_lancelot
+PRODUCT_NAME := voltage_lancelot
 PRODUCT_DEVICE := lancelot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
@@ -26,3 +26,18 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     DeviceName=lancelot \
     BuildDesc="lancelot-user 11 RP1A.200720.011 V12.5.6.0.RJCMIXM release-keys" \
     BuildFingerprint=Redmi/lancelot_global/lancelot:11/RP1A.200720.011/V12.5.6.0.RJCMIXM:user/release-keys
+
+# VoltageOS Stuffs
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+
+TARGET_BOOT_ANIMATION_RES := 1920
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+PRODUCT_NO_CAMERA := true
+
+PRODUCT_PACKAGES += \
+    Camera
+
+# include private signing keys
+-include vendor/voltage-priv/keys/keys.mk
